201741cea852f2fb8659d9ddc00c067041b4696b
tnobrega <tiago.nobrega@lasa.com.br>
2017-05-21 07:44:20 -0300

    commit inicial

---
+++ src/main/java/it/ninebee/Application.java
@@ -0,0 +1,18 @@
+package it.ninebee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
